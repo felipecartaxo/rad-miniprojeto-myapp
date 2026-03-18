abcbaecdd098d334df3bd1c410c7cf1f80224b55
felipecartaxo <felipecartaxo1@gmail.com>
2026-03-17 21:37:15 -0300

    fix: padroniza os comentarios e aspas/aspas duplas dos endpoints

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,11 @@ from . import views
 # APENAS os endpoints do app blog
 urlpatterns = [
     # Endpoint que chama a função welcome quando o usuário acessar /welcome/
-    path('welcome/', views.welcome, name='welcome'),
+    path("welcome/", views.welcome),
 
     # Endpoint que chama a função eco e que passa também um parâmetro de texto, quando o usuário acessar /eco
     path("eco/<str:text>/", views.eco),
+
+    # Endpoint que retorna o json
     path("info/", views.info)
 ]
