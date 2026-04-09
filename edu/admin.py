5ab5c721463c2dddc1c243f4b88f1dcd85a77281
felipecartaxo <felipecartaxo1@gmail.com>
2026-04-08 22:35:46 -0300

    feat: configura os models no admin e popula o banco

--- edu/admin.py
+++ edu/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Autor, Editora, Livro, Publica
 
 # Register your models here.
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('id','nome')
+    search_fields = ('nome',)
+
+@admin.register(Editora)
+class EditoraAdmin(admin.ModelAdmin):
+    list_display = ('id','nome')
+    search_fields = ('nome',)
+
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+    list_display = ('id','titulo','isbn','preco','estoque','publicacao','editora')
+    search_fields = ('titulo','isbn')
+    list_filter = ('editora',)
+
+@admin.register(Publica)
+class PublicaAdmin(admin.ModelAdmin):
+    list_display = ('id','livro','autor')
+    search_fields = ('livro__titulo','autor__nome')
