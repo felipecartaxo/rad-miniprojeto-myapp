a334157118bcdbdf5de51d5a28b2e19ec5725546
felipecartaxo <felipecartaxo1@gmail.com>
2026-04-08 22:14:06 -0300

    feat: criacao dos modelos e realizacao do migrate

--- edu/models.py
+++ edu/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+class Autor(models.Model):
+    # Semelhante ao Java, onde usamos @GeneratedValue(strategy = GenerationType.IDENTITY) para gerar o id automaticamente
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100, unique=True, null=False, blank=False)
+
+    def __str__(self):
+        return self.nome
+    
+class Editora(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100, unique=True, null=False, blank=False)
+
+    def __str__(self):
+        return self.nome
+    
+class Livro(models.Model):
+    id = models.AutoField(primary_key=True)
+    isbn = models.CharField(max_length=13, unique=True, null=False, blank=False)
+    titulo = models.CharField(max_length=20, null=False, blank=False)
+    publicacao = models.DateField(null=False, blank=False)
+    preco = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False)
+    estoque = models.IntegerField(null=False, blank=False)
+    
+    # Relacionamento 1:N com Editora
+    editora = models.ForeignKey(Editora, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.titulo} - {self.isbn} - {self.preco} - {self.estoque} - {self.publicacao}'
+
+class Publica(models.Model):
+    # Relacionamento N:N entre Livro e Autor, onde um livro pode ter vários autores e um autor pode escrever vários livros
+    livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+
+    # Similar ao Java, onde usamos @ManyToMany(mappedBy = "livros") para criar a relação muitos para muitos entre Livro e Autor
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['livro', 'autor'], name='unique_livro_autor')
+        ]
